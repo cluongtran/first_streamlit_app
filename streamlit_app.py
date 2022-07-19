8ca4abe134ef85ad9566de5916776b611f1ec95c
Christine Tran <109497541+cluongtran@users.noreply.github.com>
2022-07-18 22:52:37 -0500

    Update streamlit_app.py
    json and dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,3 +25,8 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice Fruit Advice!')
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# Pretty-fy Json Response
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Makes table header readable
+streamlit.dataframe(fruityvice_normalized)
