02ab510bd051712641e9bc29f03a7891c70fa42b
Christine Tran <109497541+cluongtran@users.noreply.github.com>
2022-07-19 00:04:16 -0500

    Update streamlit_app.py
    modify look and feel.

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,15 +25,17 @@ streamlit.dataframe(fruits_to_show)
 
 #New Section todisplay fruityvice API response
 streamlit.header('Fruityvice Fruit Advice!')
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-# Pretty-fy Json Response
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# Makes table header readable
-streamlit.dataframe(fruityvice_normalized)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    stremalit.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+    streamlit.error();
 
 streamlit.stop()
 # snowflake connector
