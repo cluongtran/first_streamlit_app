c09d737974afa54d6ad11dfc2e356d5a4f92d167
Christine Tran <109497541+cluongtran@users.noreply.github.com>
2022-07-18 22:53:44 -0500

    Update streamlit_app.py
    Add text box

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,6 +25,9 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice Fruit Advice!')
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 # Pretty-fy Json Response
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Makes table header readable
