5ace1f725eb0b8f7defbe0740b030ae7bb46214f
Christine Tran <109497541+cluongtran@users.noreply.github.com>
2022-07-19 00:11:02 -0500

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,14 +51,14 @@ streamlit.header("The fruit load list contains:")
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
     my_cur.execute("select * from fruit_load_list")
-  return my_cur.fetchall()
+    return my_cur.fetchall()
 
 # Add a button to load fruit
 if streamlit.button('Get Fruit Load List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
-
+  
 second_choice = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('Thanks for adding ', second_choice)
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
